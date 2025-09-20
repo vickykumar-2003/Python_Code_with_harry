22c5886f98099d9486e02928f650b5b9402e34ea
vickykumar-2003 <vickyk15009@gmail.com>
2025-09-20 13:34:24 +0530

    py

---
+++ conditional/03_mutiple_if_statement.py
@@ -0,0 +1,15 @@
+a = int(input("Enter your age:"))
+
+#  if statement no: 1
+if(a%2 == 0):
+    print("a is even")
+
+# if statement no: 2
+if(a>=18):
+    print("you are above the age of consent")
+    
+elif(a<0):
+    print("you are entring an invaild age")
+else:
+    print("you are below the age of consent")
+     
