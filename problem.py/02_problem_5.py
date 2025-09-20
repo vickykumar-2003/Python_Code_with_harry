a67a36f7fc7e5a288fdf4d9c2111981213e58a57
vickykumar-2003 <vickyk15009@gmail.com>
2025-09-20 11:52:56 +0530

    py

--- problem.py/02_problem_5.py
+++ problem.py/02_problem_5.py
@@ -1,23 +1,32 @@
 # s = set()
+
 # n = input("Enter number:")
 # s.add(int(n))
+
 # n = input("Enter number:")
 # s.add(int(n))
+
 # n = input("Enter number:")
 # s.add(int(n))
+
 # n = input("Enter number:")
 # s.add(int(n))
+
 # n = input("Enter number:")
 # s.add(int(n))
+
 # n = input("Enter number:")
 # s.add(int(n))
+
 # n = input("Enter number:")
 # s.add(int(n))
+
 # n = input("Enter number:")
 # s.add(int(n))
 
 # print(s)
 
+# kya hun int or str set me likh skte h
 # s = set()
 # s.add(18)
 # s.add('12')
