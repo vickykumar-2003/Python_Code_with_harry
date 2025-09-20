a400c43824cae61274c641f0d50d1380754e68e2
vickykumar-2003 <vickyk15009@gmail.com>
2025-09-20 12:58:24 +0530

    py

---
+++ conditional/01_coditional.py
@@ -0,0 +1,9 @@
+#  if else statement.....
+a = int(input("Enter your age:"))
+if(a>=18):
+    print("you are above the age of concent")
+    print("Good for you")
+else:
+    print("your are belwo the age of concent")
+    
+print("End of Program")
---
+++ conditional/02_if_elif_else_lader.py
@@ -0,0 +1,22 @@
+
+#  if elif else lader.....
+a = int(input("Enter your age:"))
+if(a>=18):
+    print("you are above the age of concent")
+    print("Good for you")
+elif(a<0):
+    print("You are entering an invalid negative age")
+elif(a==0):
+    print("You are entering 0 which is not a valid age")
+else:
+    print("your are belwo the age of concent")
+    
+print("End of Program")
+
+
+# Age greater than 18 then print Yes....
+a = int(input("Enter your age:"))
+if(a >= 18):
+    print("Yees")
+else:
+    print("NO")
