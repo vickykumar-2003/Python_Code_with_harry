f5f499ddfcd58cee45eb934bf4ce6e6a03762a22
vickykumar-2003 <vickyk15009@gmail.com>
2025-09-20 14:18:41 +0530

    py

---
+++ Loops/03_list_using_while.py
@@ -0,0 +1,6 @@
+l = [1,"vicky",False,"rohan","shubham"]
+
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i +=1
---
+++ Loops/04_for_loops.py
@@ -0,0 +1,7 @@
+# for i in range(10):
+#     print(i)
+
+
+# Range() function...
+for i in range(0,50,2):
+    print(i)
