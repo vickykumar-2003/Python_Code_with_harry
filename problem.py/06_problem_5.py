57034280567cdcf845518470a1a185cda153df1b
vickykumar-2003 <vickyk15009@gmail.com>
2025-09-20 12:12:46 +0530

    py

---
+++ problem.py/06_problem_5.py
@@ -0,0 +1,20 @@
+# same name hoga to last time wala value update ho jayega 
+d = {}
+
+name = input("Enter friends name:")
+lang = input("Enter Language name:")
+d.update({name:lang})
+
+name = input("Enter friends name:")
+lang = input("Enter Language name:")
+d.update({name:lang})
+
+name = input("Enter friends name:")
+lang = input("Enter Language name:")
+d.update({name:lang})
+
+name = input("Enter friends name:")
+lang = input("Enter Language name:")
+d.update({name:lang})
+
+print(d)
